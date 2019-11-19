134253e5bcf0b58b9df7a007f7b1d7e8553f8101
NimSubha <57932514+NimSubha@users.noreply.github.com>
2019-11-19 17:03:53 +0530

    Add files via upload

---
+++ Services/BlankOperations/CustomControls/NIM_HomeControl.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Microsoft.Dynamics.Retail.Pos.BlankOperations
+{
+    partial class NIM_HomeControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblCR4 = new System.Windows.Forms.Label();
+            this.lblC4 = new System.Windows.Forms.Label();
+            this.lblCR3 = new System.Windows.Forms.Label();
+            this.lblC3 = new System.Windows.Forms.Label();
+            this.lblCR2 = new System.Windows.Forms.Label();
+            this.lblC2 = new System.Windows.Forms.Label();
+            this.lblCR1 = new System.Windows.Forms.Label();
+            this.lblC1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCustTransDateTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick); 
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Last Updated :";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(114, 49);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(19, 17);
+            this.lblDate.TabIndex = 18;
+            this.lblDate.Text = "l1";
+            // 
+            // lblCR4
+            // 
+            this.lblCR4.AutoSize = true;
+            this.lblCR4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCR4.Location = new System.Drawing.Point(205, 28);
+            this.lblCR4.Name = "lblCR4";
+            this.lblCR4.Size = new System.Drawing.Size(13, 18);
+            this.lblCR4.TabIndex = 17;
+            this.lblCR4.Text = "-";
+            // 
+            // lblC4
+            // 
+            this.lblC4.AutoSize = true;
+            this.lblC4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblC4.Location = new System.Drawing.Point(146, 28);
+            this.lblC4.Name = "lblC4";
+            this.lblC4.Size = new System.Drawing.Size(13, 18);
+            this.lblC4.TabIndex = 16;
+            this.lblC4.Text = "-";
+            // 
+            // lblCR3
+            // 
+            this.lblCR3.AutoSize = true;
+            this.lblCR3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCR3.Location = new System.Drawing.Point(75, 28);
+            this.lblCR3.Name = "lblCR3";
+            this.lblCR3.Size = new System.Drawing.Size(13, 18);
+            this.lblCR3.TabIndex = 15;
+            this.lblCR3.Text = "-";
+            // 
+            // lblC3
+            // 
+            this.lblC3.AutoSize = true;
+            this.lblC3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblC3.Location = new System.Drawing.Point(15, 29);
+            this.lblC3.Name = "lblC3";
+            this.lblC3.Size = new System.Drawing.Size(13, 18);
+            this.lblC3.TabIndex = 14;
+            this.lblC3.Text = "-";
+            // 
+            // lblCR2
+            // 
+            this.lblCR2.AutoSize = true;
+            this.lblCR2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCR2.Location = new System.Drawing.Point(205, 8);
+            this.lblCR2.Name = "lblCR2";
+            this.lblCR2.Size = new System.Drawing.Size(13, 18);
+            this.lblCR2.TabIndex = 13;
+            this.lblCR2.Text = "-";
+            // 
+            // lblC2
+            // 
+            this.lblC2.AutoSize = true;
+            this.lblC2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblC2.Location = new System.Drawing.Point(146, 8);
+            this.lblC2.Name = "lblC2";
+            this.lblC2.Size = new System.Drawing.Size(13, 18);
+            this.lblC2.TabIndex = 12;
+            this.lblC2.Text = "-";
+            // 
+            // lblCR1
+            // 
+            this.lblCR1.AutoSize = true;
+            this.lblCR1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCR1.Location = new System.Drawing.Point(75, 8);
+            this.lblCR1.Name = "lblCR1";
+            this.lblCR1.Size = new System.Drawing.Size(13, 18);
+            this.lblCR1.TabIndex = 11;
+            this.lblCR1.Text = "-";
+            // 
+            // lblC1
+            // 
+            this.lblC1.AutoSize = true;
+            this.lblC1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblC1.Location = new System.Drawing.Point(15, 8);
+            this.lblC1.Name = "lblC1";
+            this.lblC1.Size = new System.Drawing.Size(13, 18);
+            this.lblC1.TabIndex = 10;
+            this.lblC1.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(287, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 17);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Customer last transaction :";
+            this.label2.Visible = false;
+            // 
+            // lblCustTransDateTime
+            // 
+            this.lblCustTransDateTime.AutoSize = true;
+            this.lblCustTransDateTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustTransDateTime.Location = new System.Drawing.Point(460, 49);
+            this.lblCustTransDateTime.Name = "lblCustTransDateTime";
+            this.lblCustTransDateTime.Size = new System.Drawing.Size(132, 17);
+            this.lblCustTransDateTime.TabIndex = 20;
+            this.lblCustTransDateTime.Text = "custTransDateTime";
+            this.lblCustTransDateTime.Visible = false;
+            // 
+            // NIM_HomeControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(222)))), ((int)(((byte)(229)))));
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCustTransDateTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblCR4);
+            this.Controls.Add(this.lblC4);
+            this.Controls.Add(this.lblCR3);
+            this.Controls.Add(this.lblC3);
+            this.Controls.Add(this.lblCR2);
+            this.Controls.Add(this.lblC2);
+            this.Controls.Add(this.lblCR1);
+            this.Controls.Add(this.lblC1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(24)))), ((int)(((byte)(86)))));
+            this.Name = "NIM_HomeControl";
+            this.Size = new System.Drawing.Size(596, 75);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblCR4;
+        private System.Windows.Forms.Label lblC4;
+        private System.Windows.Forms.Label lblCR3;
+        private System.Windows.Forms.Label lblC3;
+        private System.Windows.Forms.Label lblCR2;
+        private System.Windows.Forms.Label lblC2;
+        private System.Windows.Forms.Label lblCR1;
+        private System.Windows.Forms.Label lblC1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCustTransDateTime;
+    }
+}
---
+++ Services/BlankOperations/CustomControls/NIM_HomeControl.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel.Composition;
+
+using Microsoft.Dynamics.Retail.Pos.Contracts.UI;
+using LSRetailPosis.Transaction;
+using System.Data.SqlClient;
+using Microsoft.Dynamics.Retail.Pos.Contracts;
+using LSRetailPosis.Settings;
+using System.Globalization;
+
+namespace Microsoft.Dynamics.Retail.Pos.BlankOperations
+{
+    [Export(typeof(IPosCustomControl))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+
+
+    public partial class NIM_HomeControl : UserControl, IPosCustomControl
+    {
+        [Import]
+        private IApplication application;
+
+        #region enum MetalType
+        public enum MetalType
+        {
+            Other = 0,
+            Gold = 1,
+            Silver = 2,
+            Platinum = 3,
+            Alloy = 4,
+            Diamond = 5,
+            Pearl = 6,
+            Stone = 7,
+            Consumables = 8,
+            Watch = 11,
+            LooseDmd = 12,
+            Palladium = 13,
+            Jewellery = 14,
+            Metal = 15,
+            PackingMaterial = 16,
+            Certificate = 17,
+            GiftVoucher = 18,
+        }
+        #endregion
+
+        public NIM_HomeControl()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadLayout(string layoutId)
+        {
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        public void TransactionChanged(Contracts.DataEntity.IPosTransaction transaction)
+        {
+            RetailTransaction retailTransaction = transaction as RetailTransaction;
+            if (retailTransaction != null)
+                lblCustTransDateTime.Text = retailTransaction.Customer.CustomerId;
+            else
+                lblCustTransDateTime.Text = "";
+
+        }
+
+        private void ShowCurrentRate()
+        {
+            #region OLd
+            //DataTable dtRate = new DataTable();
+            //SqlCommand SqlComm = new SqlCommand();
+
+            //SqlComm.CommandType = CommandType.Text;
+
+            //SqlComm.CommandText = "SELECT top (1) RATES,CONFIGIDSTANDARD FROM METALRATES WHERE CONFIGIDSTANDARD" +
+            //                    " =(select top 1 DEFAULTCONFIGIDGOLD from INVENTPARAMETERS)" +
+            //                    " and MetalType=" + (int)MetalType.Gold + "" +
+            //                    " and RateType= " + (int)Enums.EnumClass.RateType.Sale + "" +
+            //                    " and INVENTLOCATIONID='" + ApplicationSettings.Database.StoreID +"' " +
+            //                    " and Active = 1 order by TRANSDATE desc, [Time] Desc ";
+
+            //SqlConnection connection = new SqlConnection();
+
+            //if (application != null)
+            //    connection = application.Settings.Database.Connection;
+            //else
+            //    connection = ApplicationSettings.Database.LocalConnection;
+
+            //if (connection.State == ConnectionState.Closed)
+            //    connection.Open();
+
+            //SqlComm.Connection = connection;
+
+            //SqlDataAdapter SqlDa = new SqlDataAdapter(SqlComm);
+            //SqlDa.Fill(dtRate);
+
+            //if (dtRate != null && dtRate.Rows.Count > 0)
+            //{
+            //    lblLatestRate.Text = " Today's Gold Rate" + " (" + Convert.ToString(dtRate.Rows[0]["CONFIGIDSTANDARD"]) + ")"
+            //        + System.Environment.NewLine + "           " +
+            //         Convert.ToString(decimal.Round((Convert.ToDecimal(Convert.ToString(dtRate.Rows[0]["RATES"]))), 2, MidpointRounding.AwayFromZero));
+
+            //}
+            #endregion
+
+            GetMetalRates();
+        }
+
+        private void GetMetalRates() // added on 31/03/2014 RHossain for  show the tax detail in line
+        {
+            DataTable dTMetalRates = new DataTable();
+
+            SqlConnection connection = new SqlConnection();
+            // string sMetalRates = "";
+
+            if (application != null)
+                connection = application.Settings.Database.Connection;
+            else
+                connection = ApplicationSettings.Database.LocalConnection;
+
+            if (connection.State == ConnectionState.Closed)
+                connection.Open();
+
+            //SqlTransaction sqlTrans =  connection.BeginTransaction();
+
+            string sQuery = "GETMETALRATES";
+            SqlCommand command1 = new SqlCommand(sQuery, connection);
+            command1.CommandType = CommandType.StoredProcedure;
+            command1.CommandTimeout = 0;
+            command1.Parameters.Clear();
+            command1.Parameters.Add("@STOREID", SqlDbType.NVarChar).Value = ApplicationSettings.Terminal.StoreId;
+            command1.Parameters.Add("@P_EXECSTATUS", SqlDbType.Int).Value = 0;
+
+            SqlDataAdapter daCol = new SqlDataAdapter(command1);
+            daCol.Fill(dTMetalRates);
+
+
+            if (connection.State == ConnectionState.Closed)
+                connection.Close();
+
+
+            DataTable dt = GetConfigListForRateShow();
+
+            if (dTMetalRates != null && dTMetalRates.Rows.Count > 0)
+            {
+                for (int i = 1; i <= dTMetalRates.Rows.Count; i++)
+                {
+                    //    if(i == 1)
+                    //        sMetalRates = Convert.ToString(dTMetalRates.Rows[i - 1]["CONFIGIDSTANDARD"]) + " Rates : " + Convert.ToString(dTMetalRates.Rows[i - 1]["RATES"]);
+                    //    else
+                    //        sMetalRates = sMetalRates + Environment.NewLine + Convert.ToString(dTMetalRates.Rows[i - 1]["CONFIGIDSTANDARD"]) + " Rates : " + Convert.ToString(dTMetalRates.Rows[i - 1]["RATES"]);
+                    if (dt != null && dt.Rows.Count > 0)
+                    {
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            if (Convert.ToString(dTMetalRates.Rows[i - 1]["CONFIGIDSTANDARD"]) == Convert.ToString(row["CONFIG"]))
+                            {
+                                //if(i == 1)
+                                //if (lblDate.Text == "l1")
+                                string sDC = GetDefaultConfigId();
+
+                                if (sDC == Convert.ToString(row["CONFIG"]))
+                                    lblDate.Text = Convert.ToString(dTMetalRates.Rows[i - 1]["TIMEA"]);
+
+                                if (Convert.ToInt16(row["SEQUENCE"]) == 1)
+                                {
+                                    lblC1.Text = Convert.ToString(row["CONFIG"]) + ":";
+                                    lblCR1.Text = Convert.ToString(dTMetalRates.Rows[i - 1]["RATES"]);
+                                }
+
+                                if (Convert.ToInt16(row["SEQUENCE"]) == 2)
+                                {
+                                    lblC2.Text = Convert.ToString(row["CONFIG"]) + ":";
+                                    lblCR2.Text = Convert.ToString(dTMetalRates.Rows[i - 1]["RATES"]);
+                                }
+
+                                if (Convert.ToInt16(row["SEQUENCE"]) == 3)
+                                {
+                                    lblC3.Text = Convert.ToString(row["CONFIG"]) + ":";
+                                    lblCR3.Text = Convert.ToString(dTMetalRates.Rows[i - 1]["RATES"]);
+                                }
+
+                                if (Convert.ToInt16(row["SEQUENCE"]) == 4)
+                                {
+                                    lblC4.Text = Convert.ToString(row["CONFIG"]) + ":";
+                                    lblCR4.Text = Convert.ToString(dTMetalRates.Rows[i - 1]["RATES"]);
+                                }
+
+                            }
+                        }
+                    }
+
+                    //DateTime localDate = DateTime.Now;
+
+                    //var culture = new CultureInfo(ApplicationSettings.Terminal.CultureName);
+                    //lblDate.Text = localDate.ToString(culture);
+                }
+            }
+
+            //lblC1.Text = sMetalRates;
+        }
+
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ShowCurrentRate();
+        }
+
+
+        private DataTable GetConfigListForRateShow()
+        {
+            try
+            {
+                SqlConnection SqlCon = new SqlConnection(ApplicationSettings.Database.LocalConnectionString);
+                if (SqlCon.State == ConnectionState.Closed)
+                    SqlCon.Open();
+
+                SqlCommand SqlComm = new SqlCommand();
+                SqlComm.Connection = SqlCon;
+                SqlComm.CommandType = CommandType.Text;
+                SqlComm.CommandText = "SELECT isnull(SEQUENCE,0) SEQUENCE ,isnull(CONFIG,'') CONFIG FROM RATESHOWCONFIGURATION ";
+
+                DataTable CustBalDt = new DataTable();
+
+                SqlDataAdapter SqlDa = new SqlDataAdapter(SqlComm);
+                SqlDa.Fill(CustBalDt);
+
+                if (SqlCon.State == ConnectionState.Open)
+                    SqlCon.Close();
+
+                return CustBalDt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private string GetDefaultConfigId()
+        {
+            string storeId = string.Empty;
+            SqlConnection conn = new SqlConnection(ApplicationSettings.Database.LocalConnectionString);
+
+            storeId = ApplicationSettings.Database.StoreID;
+            StringBuilder commandText = new StringBuilder();
+
+            commandText.Append(" SELECT isnull(DEFAULTCONFIGIDGOLD,'') FROM [RETAILPARAMETERS] WHERE DATAAREAID='" + ApplicationSettings.Database.DATAAREAID + "' ");
+
+
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+            SqlCommand command = new SqlCommand(commandText.ToString(), conn);
+            command.CommandTimeout = 0;
+            string sResult = Convert.ToString(command.ExecuteScalar());
+
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+
+            return Convert.ToString(sResult.Trim());
+        }
+    }
+}
